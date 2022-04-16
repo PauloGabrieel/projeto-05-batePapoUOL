d0cedecbc5915f65a3a67bd1268e05304675af22
Paulo Gabriel <gabrieeel.c@gmail.com>
2022-04-16 14:44:36 -0300

    Finalizado requisito de enviar menssagem

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
-const perguntaNomeUsuario = prompt("Olá, qual é seu nome ?"); 
-const usuario = {name: perguntaNomeUsuario};
+function logar(){
+    
+    const container = document.querySelector(".container");
+    const telaDeLogin = document.querySelector(".tela-de-login");
+    const nomeUsuario = document.querySelector(".tela-de-login input").value;
+
+    telaDeLogin.classList.add("esconder");
+    container.classList.remove("esconder");
+    entrarNoChat(nomeUsuario);
+}
+
+// const perguntaNomeUsuario = prompt("Olá, qual é seu nome ?"); 
+// const usuario = {name: perguntaNomeUsuario};
 
 
 let mensagens =[];
 
-function entrarNoChat(){
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants" , usuario);
+function entrarNoChat(nomeUsuario){
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants" , {
+        name: nomeUsuario
+    });
 
     promise.then(tratarSucesso);
     promise.catch(tratarError);
@@ -15,83 +28,107 @@ function entrarNoChat(){
 
 function tratarError(error){
        
-    // if(error.response.status == 400){
-    //     alert("Nome de usuário já cadastrado, escolha outro nome.")
-        
-    // }
+    if(error.response.status == 400){
+        alert("Nome de usuário já cadastrado, escolha outro nome.")
+        window.location.reload()   
+    }
 }
 
 function tratarSucesso(response){
     
 
 }
+// function statusConexao(){
+//     const promise = axios.post =("https://mock-api.driven.com.br/api/v6/uol/status", usuario);
+//     promise.then(manterConexao);
+//     promise.catch(errorManterConexao)
+// }
 
+// function manterConexao(response){
+//     console.log(response)
+//     console.log("teste")
+// }
 
-function buscarMensagens(){
+// function errorManterConexao(error){
+//     console.log(error)
+// }
+// function buscarMensagens(){
     
-    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-    promise.then(carregarMensagems)
-    promise.catch(tratarErrorMensagens)
-}
+//     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+//     promise.then(carregarMensagems)
+//     promise.catch(tratarErrorMensagens)
+// }
 
-function carregarMensagems(response){
-    mensagens = response.data;
-    console.log(mensagens);
-    renderizarMensagens();
+// function carregarMensagems(response){
+//     mensagens = response.data;
+//     renderizarMensagens();
 
-}
+// }
 
-function renderizarMensagens(){
-    const ulMensagens = document.querySelector('ul');
+// function renderizarMensagens(){
+//     const ulMensagens = document.querySelector('ul');
     
 
 
-    for(let index =0; index < mensagens.length;index++){
-        if( mensagens[index].type == "status"){
+//     for(let index =0; index < mensagens.length;index++){
+//         if( mensagens[index].type == "status"){
     
-            ulMensagens.innerHTML += `
-                <li class="status">
-                    <div class="horario">${mensagens[index].time}</div>
-                    <div class="remetente">${mensagens[index].from}</div> para 
-                    <div class="destinatario">${mensagens[index].to}:</div> 
-                    <div class="texto">${mensagens[index].text}</div>
-                </li>
-        `
-        }else if(mensagens[index].type == "private-message"){
-            ulMensagens.innerHTML += `
-                <li class="private-message>
-                    <div class="horario">${mensagens[index].time}</div>
-                    <div class="remetente">${mensagens[index].from}</div> para 
-                    <div class="destinatario">${mensagens[index].to}:</div> 
-                    <div class="texto">${mensagens[index].text}</div>
-                </li>
-        `
-        }else{
-            ulMensagens.innerHTML += `
-            <li class="private-message>
-                <div class="horario">${mensagens[index].time}</div>
-                <div class="remetente">${mensagens[index].from}</div> para 
-                <div class="destinatario">${mensagens[index].to}:</div> 
-                <div class="texto">${mensagens[index].text}</div>
-            </li>
-    `
-        }
-    }
+//             ulMensagens.innerHTML += `
+//                 <li class="status">
+//                     <div class="horario">${mensagens[index].time}</div>
+//                     <div class="remetente">${mensagens[index].from}</div> para 
+//                     <div class="destinatario">${mensagens[index].to}:</div> 
+//                     <div class="texto">${mensagens[index].text}</div>
+//                 </li>
+//         `
+//         }else if(mensagens[index].type == "private-message"){
+//             ulMensagens.innerHTML += `
+//                 <li class="private-message>
+//                     <div class="horario">${mensagens[index].time}</div>
+//                     <div class="remetente">${mensagens[index].from}</div> para 
+//                     <div class="destinatario">${mensagens[index].to}:</div> 
+//                     <div class="texto">${mensagens[index].text}</div>
+//                 </li>
+//         `
+//         }else{
+//             ulMensagens.innerHTML += `
+//             <li class="private-message>
+//                 <div class="horario">${mensagens[index].time}</div>
+//                 <div class="remetente">${mensagens[index].from}</div> para 
+//                 <div class="destinatario">${mensagens[index].to}:</div> 
+//                 <div class="texto">${mensagens[index].text}</div>
+//             </li>
+//     `
+//         }
+//     }
   
-    setInterval(buscarMensagens,10000);
-}    
+//     setTimeout(buscarMensagens,10000);
+// }    
 
-function tratarErrorMensagens(error){
+// function tratarErrorMensagens(error){
 
-    console.log(error.response);
+//     console.log(error.response);
 
-}
+// }
+
+// function enviarMensagem(){
+//     let mensagem = document.querySelector("input").value;
+
+//     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", {
+//         from: usuario.name,
+//         to: "todos",
+//         text: mensagem,
+//         type: "message"
+//     });
+//     promise.then(buscarMensagens);
+//     promise.catch(tratarErroEnvioMsg);
+// }
 
 
 
 
-entrarNoChat();
-buscarMensagens();
+// entrarNoChat();
+// buscarMensagens();
 
 // promise.then(EntrarNoChat)
 // function EntrarNoChat(){
